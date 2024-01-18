1f0d4f372935e0dfa214dd6d77fdb62076a68675
早起喝燕麦 <zhuowangyi@163.com>
2024-01-18 13:43:18 +0800

    vueuse/core

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -1,5 +1,38 @@
 import { login, logout, type LoginData } from '@/api/user'
-
 import { useUserStore } from '@/stores'
-
 import { setToken, clearToken } from '@/utils/token'
+import { removeRouteListener } from '@/utils/routerListener'
+
+/**
+ * @desc system authentication
+ *
+ */
+export default function useAuth() {
+  const loginApp = async (data: LoginData) => {
+    try {
+      const res = await login(data)
+      setToken(res.data.token)
+    } catch (error) {
+      clearToken()
+      throw error
+    }
+  }
+  const logoutApp = async () => {
+    const userStore = useUserStore()
+    const afterLogout = () => {
+      userStore.resetUserInfo()
+      clearToken()
+      removeRouteListener()
+    }
+    try {
+      await logout()
+    } finally {
+      afterLogout()
+    }
+  }
+
+  return {
+    loginApp,
+    logoutApp
+  }
+}
--- src/views/login/LoginForm.tsx
+++ src/views/login/LoginForm.tsx
@@ -1,13 +1,37 @@
+import { defineComponent, ref } from "vue";
 import { type LoginData } from '@/api/user'
+import useAuth from '@/hooks/auth';
+import useLoading from '@/hooks/loading';
+import { ApplicationInfo, LocalStorageKey } from '@/types/constants';
+import {
+    Button,
+    Checkbox,
+    Form,
+    Input,
+    Link,
+    Message,
+    Space,
+    Typography,
+    type FieldRule,
+    type ValidatedError
+} from '@arco-design/web-vue'
 
-import { defineComponent } from "vue";
+import { IconLock, IconUser } from '@arco-design/web-vue/es/icon'
+import { useStorage } from '@vueuse/core'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+import Logo from '@/assets/logo.svg'
+
 
 
 export default defineComponent({
     name: 'loginForm',
     setup() {
+
+
+
         return () => (
-            <div>login form</div>
+            <div>login form </div>
         )
     }
 })
