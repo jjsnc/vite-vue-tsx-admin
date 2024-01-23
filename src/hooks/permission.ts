6be1024d876eb632456ea9f9986e44af607e8f5c
早起喝燕麦 <zhuowangyi@163.com>
2024-01-23 14:54:54 +0800

    init

---
+++ src/hooks/permission.ts
@@ -0,0 +1,30 @@
+import { useUserStore } from '@/stores'
+import { get } from 'lodash'
+
+import type {
+  RouteLocationNormalizedLoaded,
+  RouteRecordNormalized,
+  RouteRecordRaw
+} from 'vue-router'
+
+export default function usePermission() {
+  const userStore = useUserStore()
+  return {
+    checkRoutePermission(
+      route: RouteRecordRaw | RouteRecordNormalized | RouteLocationNormalizedLoaded
+    ) {
+      const requiresAuth = get(route, 'meta.requiresAuth')
+      const needRoles = (get(route, 'meta.roles') || []) as string[]
+      return (
+        !requiresAuth ||
+        needRoles.length === 0 ||
+        needRoles.includes('*') ||
+        needRoles.includes(userStore.role)
+      )
+    },
+    checkButtonPermission(needPermission: string[]): boolean {
+      const userStore = useUserStore()
+      return needPermission.includes(userStore.role)
+    }
+  }
+}
--- src/router/guard/permission.ts
+++ src/router/guard/permission.ts
@@ -1,3 +1,20 @@
+import NProgress from 'nprogress' // progress bar
 import type { RouteRecord, Router } from 'vue-router'
 
-export default function setupPermissionGuard(router: Router) {}
+import usePermission from '@/hooks/permission'
+
+import { ViewNames } from '@/types/constants'
+import { firstPermissionRoute } from '@/hooks/appRoute'
+export default function setupPermissionGuard(router: Router) {
+  router.beforeEach(async (to, from, next) => {
+    const Permission = usePermission()
+    const permissionsAllow = Permission.checkRoutePermission(to as unknown as RouteRecord)
+    if (permissionsAllow) {
+      next()
+    } else {
+      const destination = firstPermissionRoute?.name || ViewNames.notFound
+      next({ name: destination })
+    }
+    NProgress.done()
+  })
+}
