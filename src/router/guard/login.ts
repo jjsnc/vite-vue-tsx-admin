8dabf52a52f682be5135d04b95a419ef357289bd
早起喝燕麦 <zhuowangyi@163.com>
2024-01-23 10:35:59 +0800

    完成登录校验逻辑的处理

--- src/router/guard/login.ts
+++ src/router/guard/login.ts
@@ -1,3 +1,47 @@
 import type { Router } from 'vue-router'
+import NProgress from 'nprogress'
+import { useUserStore } from '@/stores'
+import { isLogin } from '@/utils/token'
+import { ViewNames } from '@/types/constants'
+import useAuth from '@/hooks/auth'
 
-export default function setupUserLoginInfoGuard(router: Router) {}
+/**
+ *
+ * @desc userInfo and token guard
+ * - no token to login view
+ * - has token check userInfo
+ * - - has userInfo go
+ * - - no userInfo update info go
+ */
+export default function setupUserLoginInfoGuard(router: Router) {
+  const { logoutApp } = useAuth()
+  router.beforeEach(async (to, _form, next) => {
+    NProgress.start()
+    const userStore = useUserStore()
+    if (isLogin()) {
+      if (userStore.role) {
+        next()
+      } else {
+        try {
+          await userStore.refreshUserInfo()
+          next()
+        } catch (error) {
+          await logoutApp()
+          next({
+            name: ViewNames.login
+          })
+        }
+      }
+    } else {
+      if (to.name === ViewNames.login) {
+        next()
+        return
+      }
+      next({
+        name: ViewNames.login
+      })
+    }
+
+    console.log('我是路由跳转之前的拦截')
+  })
+}
