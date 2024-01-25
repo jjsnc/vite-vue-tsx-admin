9a9d50db9bf39b1a02c2de906562a9a7936a86a4
早起喝燕麦 <zhuowangyi@163.com>
2024-01-25 14:08:41 +0800

    init

--- src/hooks/appRoute.ts
+++ src/hooks/appRoute.ts
@@ -25,6 +25,19 @@ const routeIconMap: Record<RouteRecordName, typeof IconDashboard | undefined> = 
   [ViewNames.user]: IconUser
 }
 
+type Context = {
+  currentNode: MenuData | null
+  parent: MenuData | null
+}
+
+type MenuData = {
+  name: string
+  icon?: typeof IconDashboard
+  namePath: string[]
+  locale: string
+  localePath: string[]
+  children?: MenuData[]
+}
 /**
  * @desc convention
  *  dashboard is fixed no permission view, if you want dynamic ,use following and complete other logic
@@ -36,5 +49,70 @@ export const firstPermissionRoute = {
 }
 
 export default function useAppRoute() {
-  return '11'
+  const permission = usePermission()
+  const appRouteData = computed(() => {
+    const getMenuData = (route: RouteRecordRaw, context: Context) => {
+      const ret: MenuData = {
+        name: isString(route.name) ? route.name : '',
+        locale: typeof route.meta?.locale === 'string' ? route.meta.locale : '',
+        localePath: [],
+        namePath: []
+      }
+      ret.namePath.push(ret.name)
+      ret.localePath.push(ret.locale)
+      if (context.parent?.localePath) {
+        ret.localePath = context.parent.localePath.concat(ret.localePath)
+      }
+      if (context.parent?.namePath) {
+        ret.namePath = context.parent.namePath.concat(ret.namePath)
+      }
+      if (ret.name in routeIconMap) {
+        ret.icon = routeIconMap[ret.name]
+      }
+      return ret
+    }
+
+    const getSubMenu = (node: RouteRecordRaw, context: Context) => {
+      if (permission.checkRoutePermission(node)) {
+        const menuData = getMenuData(node, context)
+        context.currentNode = menuData
+        if (node.children === undefined) {
+          _map[menuData.name] = menuData
+          return menuData
+        } else {
+          const list: MenuData[] = []
+          for (let j = 0; j < node.children.length; j++) {
+            context.parent = menuData
+            const child = getSubMenu(node.children[j], context)
+            if (child) list.push(child)
+          }
+          if (list.length) {
+            menuData.children = list
+            _map[menuData.name] = menuData
+            return menuData
+          }
+          return null
+        }
+      } else {
+        return null
+      }
+    }
+    const _map: Record<RouteRecordName, MenuData | undefined> = {}
+    const nodeList = []
+    for (let i = 0; i < appRoutes.length; i++) {
+      const context: Context = {
+        currentNode: null,
+        parent: null
+      }
+      const menuNode = getSubMenu(appRoutes[i], context)
+      if (menuNode) {
+        nodeList.push(menuNode)
+      }
+    }
+    return { tree: nodeList, map: _map }
+  })
+
+  return {
+    appRouteData
+  }
 }
