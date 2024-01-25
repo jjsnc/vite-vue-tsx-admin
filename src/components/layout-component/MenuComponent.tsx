a0b3287f5212ef6d4fe92fb785bda4cd1d488507
早起喝燕麦 <zhuowangyi@163.com>
2024-01-25 20:04:09 +0800

    init

--- src/components/layout-component/MenuComponent.tsx
+++ src/components/layout-component/MenuComponent.tsx
@@ -13,9 +13,42 @@ export default defineComponent({
         const { t } = useI18n()
         const router = useRouter()
         const { appRouteData } = useAppRoute()
-        console.log(appRouteData.value.tree,'appRouteData.value.tree')
+        console.log(appRouteData.value.tree, 'appRouteData.value.tree')
         const openKeys = ref<string[]>([])
         const selectedKey = ref<string[]>([])
+        const renderMenuContent = () => {
+            const traverse = (routeList: any) => {
+                let list = []
+                for (let i = 0; i < routeList.length; i++) {
+                    const route = routeList[i]
+                    if (route.chileren == undefined) {
+                        list.push(
+                            <Menu.Item key={route.name as string} onClick={() => handleMenuItemClick(route)}></Menu.Item>
+                        )
+                    } else {
+                        list.push(
+                            <Menu.SubMenu
+                                key={route.name as string}
+                                v-slots={{
+                                    icon: () => h(route.icon),
+                                    title: () => t(route.locale)
+                                }}
+                            >
+
+                            </Menu.SubMenu>
+                        )
+                    }
+
+                }
+            }
+
+            return traverse(appRouteData.value.tree)
+        }
+        const handleMenuItemClick = (item: RouteRecordRaw) => {
+            router.push({
+                name: item.name
+            })
+        }
         return () => (
             <div>123</div>
         )
