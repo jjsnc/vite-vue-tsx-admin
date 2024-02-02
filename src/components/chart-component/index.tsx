0f526e85c5c572624a478a993e1552f8abfa4aae
早起喝燕麦 <zhuowangyi@163.com>
2024-02-02 10:23:47 +0800

    init

---
+++ src/components/chart-component/index.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+    name:'chartComponent',
+    setup(){
+        return ()=>(
+            <div>测试</div>
+        )
+    }
+})
--- src/views/dashboard/workplace/ContentChart.tsx
+++ src/views/dashboard/workplace/ContentChart.tsx
@@ -1,7 +1,9 @@
 import { defineComponent } from "vue";
-import { Spin, Card } from '@arco-design/web-vue'
+import { Spin, Card, Link } from '@arco-design/web-vue'
 import useLoading from '@/hooks/loading'
 import { useI18n } from 'vue-i18n'
+import ChartComponent from "@/components/chart-component";
+
 export default defineComponent({
     name: 'ContentChar',
     setup() {
@@ -12,7 +14,11 @@ export default defineComponent({
                 <Card
                     bordered={false}
                     title={t('workplace.contentData')}
+                    v-slots={{
+                        extra: () => <Link>{t('workplace.viewMore')}</Link>
+                    }}
                 >
+                   <ChartComponent />
 
                 </Card>
             </Spin>
