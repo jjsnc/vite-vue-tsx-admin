d5214ea409f53edf24869077d65a595b3cb4b0f0
早起喝燕麦 <zhuowangyi@163.com>
2024-02-20 11:09:34 +0800

    init

--- src/components/chart-component/index.tsx
+++ src/components/chart-component/index.tsx
@@ -1,11 +1,42 @@
-import { defineComponent } from "vue";
-
+import { defineComponent, nextTick, ref } from 'vue'
+import VChart from 'vue-echarts'
 
 export default defineComponent({
-    name:'chartComponent',
-    setup(){
-        return ()=>(
-            <div>测试</div>
+    name: 'chartComponent',
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {}
+            }
+        },
+        autoResize: {
+            type: Boolean,
+            default: true 
+        },
+        width: {
+            type: String,
+            default: '100%'
+        },
+        height: {
+            type: String,
+            default: '100%'
+        }
+    },
+    setup(props) {
+        const renderChart = ref<boolean>(false)
+        nextTick(() => {
+            renderChart.value = true
+        })
+        return () => renderChart.value && (
+            <VChart
+                style={{
+                    width: props.width,
+                    height: props.height
+                }}
+                option={props.options}
+                autoresize={props.autoResize}
+            ></VChart>
         )
     }
 })
--- src/views/dashboard/workplace/index.tsx
+++ src/views/dashboard/workplace/index.tsx
@@ -1,7 +1,7 @@
 import { defineComponent } from 'vue'
 
 import { ViewNames } from '@/types/constants'
-import { Space } from '@arco-design/web-vue'
+import { Space, Grid } from '@arco-design/web-vue'
 import OverView from './OverView'
 
 
@@ -13,7 +13,15 @@ export default defineComponent({
             <div class='flex'>
                 <div class={['flex-1', 'overflow-hidden']}>
                     <Space size="medium" direction="vertical" fill>
-                       <OverView></OverView>
+                        <OverView></OverView>
+                        <Grid.Row gutter={16}>
+                            <Grid.Col span={12}>
+                                123456
+                            </Grid.Col>
+                            <Grid.Col span={12}>
+                                654321
+                            </Grid.Col>
+                        </Grid.Row>
                     </Space>
                 </div>
                 <div class="w-72 ml-4">2</div>
