c217d57a3383350331f45b2d8d992ed8684cda46
早起喝燕麦 <zhuowangyi@163.com>
2024-02-23 13:25:56 +0800

    init

--- src/views/dashboard/workplace/index.tsx
+++ src/views/dashboard/workplace/index.tsx
@@ -1,7 +1,7 @@
 import { defineComponent } from 'vue'
 
 import { ViewNames } from '@/types/constants'
-import { Space, Grid } from '@arco-design/web-vue'
+import { Space, Grid, Carousel } from '@arco-design/web-vue'
 import OverView from './OverView'
 import PopularContents from '@/views/dashboard/workplace/PopularContents'
 import ContentPercentage from '@/views/dashboard/workplace/ContentPercentage'
@@ -10,6 +10,12 @@ import RightTopArea from './RightTopArea'
 export default defineComponent({
     name: ViewNames.login,
     setup() {
+        const imageSrc = [
+            'https://cdn.jsdelivr.net/gh/manyuemeiquqi/my-image-bed/dist/f7e8fc1e09c42e30682526252365be1c.jpg~tplv-uwbnlip3yd-webp.webp',
+            'https://cdn.jsdelivr.net/gh/manyuemeiquqi/my-image-bed/dist/94e8dd2d6dc4efb2c8cfd82c0ff02a2c.jpg~tplv-uwbnlip3yd-webp.webp',
+            'https://cdn.jsdelivr.net/gh/manyuemeiquqi/my-image-bed/dist/ec447228c59ae1ebe185bab6cd776ca4.jpg~tplv-uwbnlip3yd-webp.webp',
+            'https://cdn.jsdelivr.net/gh/manyuemeiquqi/my-image-bed/dist/1d1580d2a5a1e27415ff594c756eabd8.jpg~tplv-uwbnlip3yd-webp.webp'
+        ]
         return () => (
             <div class='flex'>
                 <div class={['flex-1', 'overflow-hidden']}>
@@ -28,6 +34,20 @@ export default defineComponent({
                 <div class="w-72 ml-4">
                     <Space size="medium" direction="vertical" fill>
                         <RightTopArea></RightTopArea>
+                        <Carousel
+                            class={['h-40', 'rounded', 'hidden']}
+                            indicatorType="slider"
+                            showArrow="hover"
+                            autoPlay
+                        >
+                            {imageSrc.map((src, index) => (
+                                <Carousel.Item>
+                                    <div key={index}>
+                                        <img src={src} alt="image" class="w-full" />
+                                    </div>
+                                </Carousel.Item>
+                            ))}
+                        </Carousel>
                     </Space>
                 </div>
             </div>
