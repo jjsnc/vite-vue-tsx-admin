978b38b33399f5ae45c59d0b5fc8e87925476d67
早起喝燕麦 <zhuowangyi@163.com>
2024-02-28 10:51:42 +0800

    iit

---
+++ src/views/dashboard/monitor/LiveStatus.tsx
@@ -0,0 +1,110 @@
+import { defineComponent, computed } from "vue";
+import { Card, Descriptions, Tag, Typography } from '@arco-design/web-vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+    name: 'LiveStatus',
+    setup() {
+      const { t } = useI18n()
+      const dataStatus = computed(() => [
+        {
+          label: 'mainstream',
+          value: '6 Mbps'
+        },
+        {
+          label: t('monitor.studioStatus.frameRate'),
+          value: '60'
+        },
+        {
+          label: 'hotStandby',
+          value: '6 Mbps'
+        },
+        {
+          label: t('monitor.studioStatus.frameRate'),
+          value: '60'
+        },
+        {
+          label: 'coldStandby',
+          value: '6 Mbps'
+        },
+        {
+          label: t('monitor.studioStatus.frameRate'),
+          value: '60'
+        }
+      ])
+      const dataPicture = computed(() => [
+        {
+          label: t('monitor.studioStatus.line'),
+          value: '热备'
+        },
+        {
+          label: 'CDN',
+          value: 'KS'
+        },
+        {
+          label: t('monitor.studioStatus.play'),
+          value: 'FLV'
+        },
+        {
+          label: t('monitor.studioStatus.pictureQuality'),
+          value: '原画'
+        }
+      ])
+      return () => (
+        <Card class="general-card" title={t('monitor.studioStatus.title.studioStatus')}>
+          {{
+            default: () => {
+              return (
+                <div>
+                  <Descriptions
+                    layout="horizontal"
+                    valueStyle={{
+                      whiteSpace: 'nowrap'
+                    }}
+                    labelStyle={{
+                      paddingRight: '8px'
+                    }}
+                    data={dataStatus.value}
+                    column={2}
+                  >
+                    {{
+                      label: ({ label }: { label: string }) =>
+                        ['mainstream', 'hotStandby', 'coldStandby'].includes(label) ? (
+                          <>
+                            <Typography.Text class="whitespace-nowrap  mr-1">
+                              {t(`monitor.studioStatus.${label}`)}
+                            </Typography.Text>
+                            <span>{t('monitor.studioStatus.bitRate')}:</span>
+                          </>
+                        ) : (
+                          <span>{label}:</span>
+                        )
+                    }}
+                  </Descriptions>
+                  <Typography.Title heading={6}>
+                    {t('monitor.studioStatus.title.pictureInfo')}
+                  </Typography.Title>
+                  <Descriptions
+                    v-slots={{
+                      label({ label }: any) {
+                        return <span>{label}:</span>
+                      }
+                    }}
+                    labelStyle={{
+                      paddingRight: '8px'
+                    }}
+                    layout="horizontal"
+                    data={dataPicture.value}
+                    column={2}
+                  />
+                </div>
+              )
+            },
+            extra: () => {
+              return <Tag color="green">{t('monitor.studioStatus.smooth')}</Tag>
+            }
+          }}
+        </Card>
+      )
+    }
+  })
--- src/views/dashboard/monitor/index.tsx
+++ src/views/dashboard/monitor/index.tsx
@@ -3,7 +3,7 @@ import { ViewNames } from '@/types/constants'
 import { Grid, Space } from '@arco-design/web-vue'
 import ChatPanel from './ChatPanel'
 import LivePanel from './LivePanel'
-
+import LiveStatus from './Livestatus'
 export default defineComponent({
     name: ViewNames.login,
     setup() {
@@ -11,13 +11,15 @@ export default defineComponent({
         return () => (
             <Grid.Row gutter={16}>
                 <Grid.Col flex="300px">
-                  <ChatPanel></ChatPanel>
+                    <ChatPanel></ChatPanel>
                 </Grid.Col>
                 <Grid.Col flex={1}>
-                     <LivePanel></LivePanel>
+                    <LivePanel></LivePanel>
                 </Grid.Col>
                 <Grid.Col flex="280px">
-                    右侧
+                    <Space fill direction="vertical" size="medium">
+                        <LiveStatus />
+                    </Space>
                 </Grid.Col>
             </Grid.Row>
         )
