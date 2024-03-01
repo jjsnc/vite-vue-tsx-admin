1ada7957fddc996283c60f38b423b7f317297b60
早起喝燕麦 <zhuowangyi@163.com>
2024-03-01 15:22:44 +0800

    init

--- src/views/list/card-list/index.tsx
+++ src/views/list/card-list/index.tsx
@@ -1,10 +1,21 @@
 import { defineComponent } from "vue";
+import { Card, Input, Tabs, Typography } from '@arco-design/web-vue'
 
+import { useI18n } from 'vue-i18n'
 export default defineComponent({
     name: 'card-list',
     setup() {
+        const { t } = useI18n()
+        
         return () => (
-            <div> card-list</div>
+            <div>
+                <Card
+                    class="general-card"
+                    v-slots={{
+                        title: () => <Typography.Title heading={6}>{t('menu.list.cardList')}</Typography.Title>
+                    }}
+                ></Card>
+            </div>
         )
     }
 })
