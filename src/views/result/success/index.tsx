077eb43bb329e6654091f2cc6c259833d8e80686
早起喝燕麦 <zhuowangyi@163.com>
2024-03-05 13:32:25 +0800

    success

--- src/views/result/success/index.tsx
+++ src/views/result/success/index.tsx
@@ -1,10 +1,66 @@
-import { defineComponent } from "vue";
-
+import { defineComponent, computed } from "vue";
+import CardLayout from '@/components/card-layout'
+import { Button, Result, Space, Steps, Typography } from '@arco-design/web-vue'
+import { useI18n } from 'vue-i18n'
 export default defineComponent({
     name: 'success',
     setup() {
+        const { t } = useI18n()
+        const stepList = computed(() => {
+            return [
+                {
+                    title: t('success.submitApplication'),
+                    description: '2020/10/10 14:00:39'
+                },
+                {
+                    title: t('success.leaderReview'),
+                    description: t('success.processing')
+                },
+                {
+                    title: t('success.purchaseCertificate'),
+                    description: t('success.waiting')
+                },
+                {
+                    title: t('success.safetyTest'),
+                    description: t('success.waiting')
+                },
+                {
+                    title: t('success.launched'),
+                    description: t('success.waiting')
+                }
+            ]
+        })
         return () => (
-            <div>666</div>
+            <div>
+                <CardLayout>
+                    <Result
+                        title={t('success.result.title')}
+                        subtitle={t('success.result.subTitle')}
+                        status="success"
+                    >
+                        {{
+                            extra: () => (
+                                <Space size="medium">
+                                    <Button type="secondary">{t('success.result.printResult')}</Button>
+                                    <Button type="primary">{t('success.result.projectList')}</Button>
+                                </Space>
+                            ),
+                            default: () => (
+                                <div class={['bg-[rgb(var(--gray-1))]', 'mt-10', 'min-w-fit', 'p-5']}>
+                                    <Typography.Paragraph>{t('success.result.progress')}</Typography.Paragraph>
+                                    <Steps type="dot" current={2}>
+                                        {stepList.value.map((item) => {
+                                            return (
+                                                <Steps.Step title={item.title} description={item.description}></Steps.Step>
+                                            )
+                                        })}
+                                    </Steps>
+                                </div>
+                            )
+                        }}
+                    </Result>
+                </CardLayout>
+            </div>
         )
     }
 })
